ea1c6502d2e790a8b88e398e5b42205f560bdca7
Donovan Allen <dallen@dallen.xyz>
2015-06-29 14:56:02 -0600

    Dallen returns from the land of no internet, he has brought pvp

---
+++ src/main/java/com/mcmiddleearth/mcme/events/PVP/Handlers/JoinLeaveHandler.java
@@ -0,0 +1,36 @@
+/*
+ * This file is part of MCME-Events.
+ * 
+ * MCME-Events is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * MCME-Events is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with MCME-Events.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * 
+ */
+package com.mcmiddleearth.mcme.events.PVP.Handlers;
+
+import com.mcmiddleearth.mcme.events.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+/**
+ *
+ * @author Donovan <dallen@dallen.xyz>
+ */
+public class JoinLeaveHandler implements Listener{
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e){
+        e.getPlayer().teleport(Bukkit.getWorld(Main.getPlugin().getSpawnWorld()).getSpawnLocation());
+    }
+}
